e751e538fcd7b7a5db37ba2d96fd5e315724fc2b
Ryan Kanno <ryankanno@localkinegrinds.com>
2012-03-14 10:11:21 -1000

    Updated django view decorator to do the right thing Added more notes to the Playground README

--- views/decorators.py
+++ views/decorators.py
@@ -11,11 +11,21 @@ def render(template=None, mimetype=None):
         @wraps(func)
         def wrapper(request, *args, **kwargs):
             out = func(request, *args, **kwargs)
+
             if not isinstance(out, dict):
                 return out
-            # Define a template dynamically in the decorator call
-            template = out.pop('TEMPLATE', template)
-            return render_to_response(template, out,
+            
+            if template and out.pop('TEMPLATE'):
+                raise Exception, "Naughty, naughty.  You've declared a template within the decorator and function." 
+
+            # Define a template dynamically in the decorated call
+            tmpl_to_render = template or out.pop('TEMPLATE', None)
+
+            # If it's still null, try to do the right thing
+            if not tmpl_to_render:
+                tmpl_to_render = "%s/%s.html" % (func.__module__.split('.')[k], func.__name__)
+
+            return render_to_response(tmpl_to_render, out,
                    context_instance=RequestContext(request), mimetype=mimetype)
         return wrapper
     return renderer
