309c4f43717ac127805664d1aa9493601fbe307d
Ryan <ryankanno@localkinegrinds.com>
2016-07-03 01:53:29 -0400

    Changed User to settings.AUTH_USER_MODEL

--- django_utilities/models/mixins.py
+++ django_utilities/models/mixins.py
@@ -2,22 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 from ..db.fields import CreatedDateTimeField
 from ..db.fields import ModifiedDateTimeField
-import datetime
 
 
 class CreatedModifiedMixin(models.Model):
 
     modified_at = ModifiedDateTimeField()
     modified_by = models.ForeignKey(
-        User, related_name='modified_by_%(class)s_set', null=True, blank=True)
+        settings.AUTH_USER_MODEL,
+        related_name='modified_by_%(class)s_set', null=True, blank=True)
 
     created_at = CreatedDateTimeField()
     created_by = models.ForeignKey(
-        User, related_name='created_by_%(class)s_set', null=True, blank=True)
+        settings.AUTH_USER_MODEL,
+        related_name='created_by_%(class)s_set', null=True, blank=True)
 
     class Meta:
         abstract = True
